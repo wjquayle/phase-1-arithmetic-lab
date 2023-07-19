bc62914351802c7596f840a6da33f6657390362c
BillQuayle <wjquayle@gmail.com>
2023-07-19 00:25:36 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2; 
+const multiply= num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 8;
+let num4 = 2;
+let mod = num3/num4;
+let max =Math.max(5, 10, 15,  20 , 1);
